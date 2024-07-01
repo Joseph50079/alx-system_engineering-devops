02e951972b3e7e9706de751cf15cb6c70fbebbfb
Joseph50079 <josephogbah2019@gmail.com>
2024-07-01 00:22:53 +0000

    lets see

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # configuring ssh with puppet
 
 augeas {'ssh_config':
-	context	=> '/etc/ssh/ssh-config',
-	changes => [
-		'set PasswordAuthentication no',
-		'set IdentifyFiles ~/.ssh/school',
-		],
-	}
+  context => '/etc/ssh/ssh-config',
+  changes => [
+    'set PasswordAuthentication no',
+    'set IdentifyFiles ~/.ssh/school',
+    ],
+  }
