f5e1a22a8f84836ead4f994b5a1088cc69f05148
Joseph50079 <josephogbah2019@gmail.com>
2024-07-01 00:46:47 +0000

    lets gooooo!!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,26 @@
-# configuring ssh with puppet
+# configuring ssh with puppe
 
-augeas {'ssh_config':
-  context => '/etc/ssh/ssh-config',
-  changes => [
-    'set PasswordAuthentication no',
-    'set IdentityFile ~/.ssh/school',
-    ],
+# ssh_client_config.pp
+
+node 'default' {
+  $ssh_config_path = '/etc/ssh/ssh_config'
+  $private_key_path = '~/.ssh/school'
+
+  # Ensure the ssh_config file exists with the desired content and correct permissions
+  file { $ssh_config_path:
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
+
+  # Use augeas to edit the ssh_config file
+  augeas { 'ssh_config':
+    context => '/files/etc/ssh/ssh_config',
+    changes => [
+      'set Host/*/IdentityFile $private_key_path',
+      'set Host/*/PasswordAuthentication no',
+    ],
+    onlyif  => 'match Host/*/IdentityFile[.="~/.ssh/school"] size == 0',
+  }
+}
