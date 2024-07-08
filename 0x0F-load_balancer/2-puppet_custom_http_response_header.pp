875b38c12c40c101195d390cb8e3ea620ad45c5e
Joseph50079 <josephogbah2019@gmail.com>
2024-07-09 00:55:10 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,25 @@
-greet
+package { 'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
+  name     => 'nginx',
+}
+exec { 'write file':
+  path    => '/usr/bin/',
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  creates => '/var/www/html/',
+}
+
+file { '/var/www/html/index.html' :
+  content => 'Hello World!',
+}
+file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+service { 'start':
+  ensure => running,
+  name   => 'nginx',
+}
