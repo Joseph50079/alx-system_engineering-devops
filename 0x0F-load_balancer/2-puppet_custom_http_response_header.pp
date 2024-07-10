1de5399f58a727a1d88a438ef0c6a5989199fb6c
Joseph50079 <josephogbah2019@gmail.com>
2024-07-10 14:29:43 +0000

    2 done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,54 @@
 #setup web server
-package { 'nginx':
-  ensure   => 'installed',
-  provider => 'apt',
-  name     => 'nginx',
-}
-exec { 'write file':
-  path    => '/usr/bin/',
-  command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  creates => '/var/www/html/',
-}
+class custom_http_header {
+  $hostname = $facts['networking']['hostname']
 
-file { '/var/www/html/index.html' :
-  content => 'Hello World!',
-}
+  # Ensure the necessary packages are installed
+  package { 'nginx':
+    ensure => installed,
+  }
 
-file {'/etc/nginx/sites-available/default':
-  content => 'server {
+  # Ensure the directory for the HTML file exists
+  file { '/var/www/html':
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+
+  # Create the HTML file with the "Hello World!" content
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0644',
+  }
+
+  # Get the hostname and configure the Nginx server block
+  exec { 'configure_nginx':
+    path    => '/usr/bin/',
+    command => "printf '%s\\n''server {
     listen 80;
     listen [::]:80 default_server;
     root   /var/www/html;
     index  index.html index.htm;
 
     location / {
-        add_header X-Served-By "$(hostname)";
+        add_header X-Served-By '${hostname}';
     }
-}',
+}
+' > /etc/nginx/sites-available/default",
+    require => Package['nginx'],
+  }
+
+  # Ensure the Nginx service is running and restart if necessary
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => Exec['configure_nginx'],
+  }
+
 }
 
-service { 'nginx':
-  ensure => running,
-}
+# Include the custom_http_header class
+include custom_http_header
