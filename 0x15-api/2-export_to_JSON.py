9a6d5b2ba375e95cdf611ed507d34217c4f6ea88
Joseph50079 <josephogbah2019@gmail.com>
2024-07-30 02:06:43 +0100

    json api

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Module for gathering data from an api"""
+
+import json
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    employee_url = f"https://jsonplaceholder.typicode.com/users?id={user_id}"
+
+    try:
+        with urllib.request.urlopen(employee_url) as emp_res:
+            user_data = emp_res.read().decode('utf-8')
+            users = json.loads(user_data)
+            EMPLOYEE_NAME = users[0]['name']
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode('utf-8')
+            todos = json.loads(data)
+
+            TOTAL_NUMBER = len(todos)
+            NUMBER = sum(todo['completed'] for todo in todos)
+            shorten = f"done with tasks({NUMBER}/{TOTAL_NUMBER}):"
+
+    except urllib.error.URLError as e:
+        print()
+    except ValueError:
+        print()
+    dicit = {}
+    lis = []
+    try:
+        for todo in todos:
+            dicit['task'] = todo['title']
+            dicit['completed'] = todo['completed']
+            dicit['username'] = users[0]['username']
+            lis.append(dicit)
+        json_data = {}
+        json_data[f"{user_id}"] = lis
+
+        json_file = f"{user_id}.json"
+        with open(json_file, mode='w') as file:
+            json.dump(json_data, file)
+    except IOError:
+        print()
